555d57b9f520a36a96580e487eec171b07dd23ea
Abhilash-pro <abhilash.3580.verma@gmail.com>
2019-12-15 15:02:01 +0530

    added cardList component

--- ImageFeed/App.js
+++ ImageFeed/App.js
@@ -1,21 +1,26 @@
 import React from 'react';
-import {View,StyleSheet} from 'react-native';
-import AuthorRow from './components/AuthorRow';
+import { View,StyleSheet} from 'react-native';
 import Card from './components/Card';
+import CardList from './components/CardList';
+
+
+const items  = [
+  {id :0 , author : 'Bob rose'},
+{id :1 , author : 'Chuck norris'},
+];
+
+
+
+
+
 
 export default class App extends React.Component {
   render (){
     return (
-      <View style ={styles.container} >
-        <Card
-        fullname ={'first Last'}
-        linkText = {'Comments'}
-        onPressLinkText = {() => {
-          console.log('Pressed link!');
-        }}
-        image ={{uri: 'https://unsplash.it/600/600'}}
-        />
-      </View>
+     
+        <View style = {styles.container}>
+          <CardList items ={items} />
+        </View>
     );
   }
 }
