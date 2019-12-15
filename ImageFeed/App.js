2719b6b13b0fee43803ea2734c6bd8b249a602ee
Abhilash-pro <abhilash.3580.verma@gmail.com>
2019-12-15 16:05:35 +0530

    added flatlist and cardlist  component
    rendering list of images from URI

--- ImageFeed/App.js
+++ ImageFeed/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
 import { View,StyleSheet} from 'react-native';
-import Card from './components/Card';
 import CardList from './components/CardList';
 
-
-const items  = [
-  {id :0 , author : 'Bob rose'},
-{id :1 , author : 'Chuck norris'},
-];
-
-
-
-
-
+import Feed from './screens/Feed';
 
 export default class App extends React.Component {
   render (){
     return (
      
         <View style = {styles.container}>
-          <CardList items ={items} />
+          <Feed style ={styles.feed} />
         </View>
     );
   }
@@ -30,5 +20,9 @@ const styles = StyleSheet.create({
   container:{
     flex: 1,
    backgroundColor: '#fff',
+  },
+  feed:{
+    flex: 1,
+    marginTop:  1,
   },
 });
